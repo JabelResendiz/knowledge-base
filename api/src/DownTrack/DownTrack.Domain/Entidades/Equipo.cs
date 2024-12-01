8ad62035a6098cc4c1be8002fcd033aa2e3d7c7b
JabelResendiz <jabelresendiz18@gmail.com>
2024-12-01 15:50:35 -0500

    entidades

---
+++ api/src/DownTrack/DownTrack.Domain/Entidades/Equipo.cs
@@ -0,0 +1,17 @@
+
+
+// Entidad Equipo 
+public class Equipo
+{
+    public int Id {get;set;}
+    public string Name{get;set;}
+
+    public string Tipo {get;set;}
+
+    public string Estado{get;set;}
+
+    public string Ubicacion{get;set;}
+
+    public DateTime FechaAdq {get;set;}
+
+}
